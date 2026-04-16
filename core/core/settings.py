7d57a683aa152dc9ae89a409a1e8ab76e344eb31
vanshlilani <vansh.l@ahduni.edu.in>
2026-04-17 01:44:02 +0530

    update

--- core/core/settings.py
+++ core/core/settings.py
@@ -30,11 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = [
-    'cross-chain-asset-simulator.onrender.com',
-    'localhost',
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
